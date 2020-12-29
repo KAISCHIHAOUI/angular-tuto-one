b56b74a68f73d2633591c5efc9bb8f3dbe43f07b
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2020-12-29 22:11:57 +0100

    3 files added

---
+++ src/app/test-interpo/test-interpo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-interpo',
+  templateUrl: './test-interpo.component.html',
+  styleUrls: ['./test-interpo.component.css']
+})
+export class TestInterpoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
