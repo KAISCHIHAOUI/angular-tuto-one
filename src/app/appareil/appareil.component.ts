8eb885cd7ce5219d00c3f8fc8bd5f9681db7435f
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2020-12-29 23:13:55 +0100

    4 files modified

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-appareil',
-  templateUrl: './appareil.component.html',
-  styleUrls: ['./appareil.component.css']
+  selector: "app-appareil",
+  templateUrl: "./appareil.component.html",
+  styleUrls: ["./appareil.component.css"]
 })
 export class AppareilComponent implements OnInit {
+  appareilName: String = "Laptop";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
