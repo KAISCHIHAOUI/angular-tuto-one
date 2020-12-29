3115a99efe5c73e348b45ca6b2124ccecc76f090
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2020-12-29 23:56:34 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
-import { AppareilComponent } from './appareil/appareil.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AppareilComponent } from "./appareil/appareil.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MonPremierComponent, AppareilComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, AppareilComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
