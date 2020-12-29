117a66179303f92c98ac611846f632f8cb074ea2
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2020-12-29 23:00:47 +0100

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MonPremierComponent, AppareilComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
