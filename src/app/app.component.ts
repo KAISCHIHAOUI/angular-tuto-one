87161d12ee7cf0a883f0dfc5daa3c58efe9ba206
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2021-01-26 14:09:32 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,30 @@
 import { Component, OnInit, VERSION } from "@angular/core";
-import { AppareilService } from "./services/appareil.service";
+import { interval, Subscription } from "rxjs";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  secondes: number;
+  counterSubscription: Subscription;
+
+  ngOnInit() {
+    //create observable
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      value => {
+        this.secondes = value;
+      },
+      error => {
+        console.log("An error occured ! : " + error);
+      },
+      () => {
+        console.log("Observable complete ! ");
+      }
+    );
+  }
 
   name = "TRAINING WITH Angular+";
-  /*
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(() => {
-      resolve(date);
-    }, 2000);
-  });
-
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 4000);
-  }
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    if (confirm("Etes vous sur d'allumer tous les appareils ?")) {
-      this.appareilService.switchOnAll();
-    } else {
-      return null;
-    }
-  }
-  onEteindre() {
-    this.appareilService.switchOffAll();
-  }*/
 }
